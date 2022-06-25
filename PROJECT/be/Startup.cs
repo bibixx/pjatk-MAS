e62dc365b00e697b9884c540baefa21108b7c204
Bartosz Legięć <bartosz@legiec.io>
2022-06-25 19:18:08 +0200

    Dockerize project

--- PROJECT/be/Startup.cs
+++ PROJECT/be/Startup.cs
@@ -50,10 +50,11 @@ namespace mas_project
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MainDbContext dataContext)
         {
             if (env.IsDevelopment())
             {
+                dataContext.Database.Migrate();
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "mas_project v1"));
